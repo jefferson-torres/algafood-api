b5d7d565a00ba13e9da54a2253d3e2456ecae979
Jefferson Torres <jeferson.torres@hotmail.com>
2022-09-04 18:17:51 -0300

    3.17. Mapeando relacionamento com @ManyToOne

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,4 +25,7 @@ public class Restaurante {
 	private String nome;
 	
     private BigDecimal taxaFrete;
+    
+    @ManyToOne
+    private Cozinha cozinha;
 }

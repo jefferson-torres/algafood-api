72c2decdc6aaecccbfc47de7bbe90b999ea901dc
Jefferson Torres <jeferson.torres@hotmail.com>
2022-09-04 17:23:31 -0300

    3.4. Mapeando entidades com JPA

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Data
+public class Cozinha {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Include
+	private Long id;
+	
+	private String nome;
+}
